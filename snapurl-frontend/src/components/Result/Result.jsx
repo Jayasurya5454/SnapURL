276f3d5004e366a0f87f0de4a5b071591771c6ab
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2025-02-15 14:39:39 +0530

    user review added

--- snapurl-frontend/src/components/Result/Result.jsx
+++ snapurl-frontend/src/components/Result/Result.jsx
@@ -4,10 +4,12 @@ import { Button } from "react-bootstrap";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Confetti from "react-confetti";
+import axios from "axios";
 import backgroundImage from "../../assets/backgroundUrl.png";
 import githubLogo from '../../assets/githublogo.png';
 import linkedinLogo from '../../assets/linkedin.svg';
 import mediumLogo from '../../assets/medium-svgrepo-com.svg';
+import { AiFillHeart } from "react-icons/ai";
 
 const Result = () => {
     const location = useLocation();
@@ -15,8 +17,58 @@ const Result = () => {
     const { shortUrl, qrCode, userId } = location.state || {};
     const formattedShortUrl = shortUrl ? `https://snappedurl.onrender.com/${shortUrl}` : null;
     const [showConfetti, setShowConfetti] = useState(true);
+    const [likeCount, setLikeCount] = useState(
+      localStorage.getItem('likeCount') ? Number(localStorage.getItem('likeCount')) : 0
+    ); 
 
-    const downloadAsPdf = () => {
+    useEffect(() => {
+        const fetchLikeCount = async () => { 
+          try {
+            const response = await axios.get('https://likeme-backend-fxrs.onrender.com/api/snapurl/count');
+            setLikeCount(response.data.count);
+            localStorage.setItem('likeCount', response.data.count);
+          } catch (error) {
+            console.error('Error fetching like count:', error);
+          }
+        };
+        fetchLikeCount();
+      }, []);
+
+    const handleLike = async () => {
+        try {
+          const response = await axios.put('https://likeme-backend-fxrs.onrender.com/api/snapurl/updatecount');
+          if (response.status === 200) {
+            const likeResponse = await axios.get('https://likeme-backend-fxrs.onrender.com/api/snapurl/count');
+            setLikeCount(likeResponse.data.count);
+            localStorage.setItem('likeCount', likeResponse.data.count);
+            toast.success('Thanks for your Support!', {
+              position: 'bottom-right',
+              autoClose: 3000,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              icon: '❤️',
+            });
+          } else {
+            throw new Error('Failed to update like count.');
+          }
+        } catch (error) {
+          console.error('Error updating like count:', error.response || error.message || error);
+          toast.error('Something went wrong! Please try again later.', {
+            position: 'bottom-right',
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }
+      };
+
+    const downloadQrCode = () => {
         const qrImage = document.createElement("a");
         qrImage.href = qrCode; 
         qrImage.download = "Snapurl.png"; 
@@ -46,7 +98,6 @@ const Result = () => {
         return () => clearTimeout(timer);
     }, []);
 
-    // Dynamic card width based on URL length
     const cardWidth = Math.max(500, Math.max(300, formattedShortUrl ? formattedShortUrl.length * 5 : 250));
 
     return (
@@ -114,9 +165,7 @@ const Result = () => {
                             >
                                 📋
                             </button>
-                            
                         </div>
-                       
                         <div
                             style={{
                                 marginBottom: "20px",
@@ -206,7 +255,7 @@ const Result = () => {
                                     marginRight: "10px",
                                     cursor: "pointer",
                                 }}
-                                onClick={() => downloadAsPdf()}
+                                onClick={downloadQrCode}
                             >
                                 Download QR
                             </Button>
@@ -223,38 +272,102 @@ const Result = () => {
                                 Open Snaped URL
                             </Button>
                             {userId && (
-                                <Button
-                                    style={{
-                                        background: "#1abc9c",
-                                        border: "none",
-                                        padding: "10px 20px",
-                                        borderRadius: "30px",
-                                        cursor: "pointer",
-                                        marginLeft: "10px",
-                                        position: "relative",
-                                    }}
-                                    onClick={handleAnalytics}
-                                >
-                                    Analytics
-                                    <div
+                                <>
+                                    <Button
                                         style={{
-                                            position: "absolute",
-                                            top: "-10px",
-                                            right: "-10px",
-                                            background: "#e74c3c",
-                                            borderRadius: "50%",
-                                            width: "20px",
-                                            height: "20px",
-                                            display: "flex",
-                                            justifyContent: "center",
-                                            alignItems: "center",
-                                            color: "#fff",
-                                            fontSize: "12px",
+                                            background: "#1abc9c",
+                                            border: "none",
+                                            padding: "10px 20px",
+                                            borderRadius: "30px",
+                                            cursor: "pointer",
+                                            marginLeft: "10px",
+                                            position: "relative",
                                         }}
+                                        onClick={handleAnalytics}
                                     >
-                                        1
+                                        Analytics
+                                        <div
+                                            style={{
+                                                position: "absolute",
+                                                top: "-10px",
+                                                right: "-10px",
+                                                background: "#e74c3c",
+                                                borderRadius: "50%",
+                                                width: "20px",
+                                                height: "20px",
+                                                display: "flex",
+                                                justifyContent: "center",
+                                                alignItems: "center",
+                                                color: "#fff",
+                                                fontSize: "12px",
+                                            }}
+                                        >
+                                            1
+                                        </div>
+                                    </Button>
+                                    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                                        <button
+                                            className="like-button"
+                                            onClick={handleLike}
+                                            style={{
+                                                position: 'relative',
+                                                display: 'flex',
+                                                alignItems: 'center',
+                                                justifyContent: 'center',
+                                                background: '#1abc9c',
+                                                border: 'none',
+                                                borderRadius: '50px',
+                                                padding: '10px 20px',
+                                                cursor: 'pointer',
+                                                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                                                transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+                                                color: 'white',
+                                                overflow: 'hidden',
+                                            }}
+                                            onMouseEnter={(e) => e.target.style.transform = 'scale(1.05)'}
+                                            onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
+                                            onMouseDown={(e) => e.target.style.transform = 'scale(0.95)'}
+                                            onMouseUp={(e) => e.target.style.transform = 'scale(1)'}
+                                        >
+                                            <AiFillHeart
+                                                style={{
+                                                    fontSize: '32px',
+                                                    marginRight: '8px',
+                                                    marginLeft: '5px',
+                                                    color: 'red',
+                                                    animation: 'pulse 1s infinite',
+                                                }}
+                                            />
+                                            <h2
+                                                style={{
+                                                    fontSize: '26px',
+                                                    fontWeight: 'bold',
+                                                    margin: '0',
+                                                    color: 'white',
+                                                }}
+                                            >
+                                                {likeCount}
+                                            </h2>
+
+                                            {/* Glow effect */}
+                                            <div
+                                                style={{
+                                                    position: 'absolute',
+                                                    top: '7',
+                                                    left: '0',
+                                                    right: '0',
+                                                    bottom: '0',
+                                                    borderRadius: '50px',
+                                                    background: '#1abc9c',
+                                                    zIndex: '-1',
+                                                    opacity: '0',
+                                                    transition: 'opacity 0.3s ease',
+                                                }}
+                                                className="glow-effect"
+                                            />
+                                        </button>
                                     </div>
-                                </Button>
+                                </>
                             )}
                         </div>
                     </>
