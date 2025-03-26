fd555e86b6779ba55eb7814bafbfa0487ae84645
Jayasurya5454 <jayasuryakarthi55@gmail.com>
2025-03-26 07:54:33 +0530

    1.1.3

--- snapurl-frontend/src/components/logins/firebase.js
+++ snapurl-frontend/src/components/logins/firebase.js
@@ -14,5 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider(); 
-
+googleProvider.setCustomParameters({
+  prompt: "select_account", 
+});
 export { auth, googleProvider };
